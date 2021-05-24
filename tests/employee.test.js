c2af631a2deca478b5330b7854d64a3e9b09ea72
Yuri Kitchin <ykitchin@gmail.com>
2021-05-24 15:19:40 +0800

    test id is number

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -1,8 +1,10 @@
 const { expect } = require('@jest/globals');
+const { number } = require('yargs');
 const employee = require('../lib/employee.js');
  
 describe('employee', () => {
   it('Should create an employee object containing name, id, and email', () => {
    const employeeTest = new employee("yuri", 11, 'ykitchin@gmail.com');
    expect(employeeTest.name).toEqual(expect.any(String))
+   expect(employeeTest.id).toEqual(expect.any(Number))
   });})
