0b0dd653fcc431703582b8ad1438a8b60618c63a
Yuri Kitchin <ykitchin@gmail.com>
2021-05-24 15:14:38 +0800

    add test to see if employee.name = string

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -1,7 +1,8 @@
+const { expect } = require('@jest/globals');
 const employee = require('../lib/employee.js');
  
 describe('employee', () => {
   it('Should create an employee object containing name, id, and email', () => {
-    expect(fizzBuzz([1])).toBe('1');
-    expect(fizzBuzz([1, 2])).toBe('1, 2');
+   const employeeTest = new employee("yuri", 11, 'ykitchin@gmail.com');
+   expect(employeeTest.name).toEqual(expect.any(String))
   });})
