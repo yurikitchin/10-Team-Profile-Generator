688e82a4be4ec158f1a6fb549bd8237d6640343b
Yuri Kitchin <ykitchin@gmail.com>
2021-05-26 19:47:56 +0800

    test to see if engineer class returns object

--- tests/engineer.test.js
+++ tests/engineer.test.js
@@ -1,7 +1,7 @@
 const { expect } = require('@jest/globals');
 const engineer = require('../lib/engineer.js');
  
-describe('employee', () => {
+describe('engineer', () => {
   it('Should create an engineer object containing name, id, email and github', () => {
    const engineerTest = new engineer("yuri", 11, 'ykitchin@gmail.com', "github");
    expect(engineerTest.name).toEqual(expect.any(String))
