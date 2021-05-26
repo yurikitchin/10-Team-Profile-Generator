43ceb60a389026854dd131344aa7a0526d76dff8
Yuri Kitchin <ykitchin@gmail.com>
2021-05-26 18:12:02 +0800

    add test's for get name mehtod and get id method

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -9,3 +9,17 @@ describe('employee', () => {
    expect(employeeTest.id).toEqual(expect.any(Number))
    expect(employeeTest.email).toEqual(expect.any(String))
   });})
+
+  describe('employee', () =>{
+    it('the getName() method should return the emplyee name as a string', () => {
+      const employeeTest = new employee("yuri", 11, 'ykitchin@gmail.com');
+      expect(employeeTest.getName()).toBeDefined()
+    })
+  })
+
+  describe('employee', () =>{
+    it('the getID() method should return the emplyee ID as an Integer', () => {
+      const employeeTest = new employee("yuri", 11, 'ykitchin@gmail.com');
+      expect(employeeTest.getName()).toBeDefined()
+    })
+  })
