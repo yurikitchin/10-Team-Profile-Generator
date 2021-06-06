4fbf2e74b787fb2214f4de4cb0047bccaca99cfa
Yuri Kitchin <ykitchin@gmail.com>
2021-06-06 15:18:15 +0800

    change inqurier questions to functions

--- index.js
+++ index.js
@@ -2,13 +2,52 @@ const inqurier = require('inquirer')
 const fs = require('fs')
 const Choices = require('inquirer/lib/objects/choices')
 //add inqurier questions that take name, id, email, officeNum for office manager
-const employeeQuestions = [
+
+
+const managerQuestions = () => {
+    inqurier.prompt ([
+        {
+            type: "input",
+            message: "please enter Managers's name",
+            name: "name",
+    
+        },
+        {
+            type: "input",
+            message: "please enter your office number",
+            name: "officeNum"
+        },
+        {
+            type: "input",
+            message: "Please enter employee ID",
+            name: "employeeID"
+        },
+        {
+            type: "input",
+            message: "please employee email",
+            name: "email"
+        }
+    ])
+    .then(managerData => {
+        console.log("this works")
+        console.log(managerData)
+    })
+   
+}
+
+const engineerQuestions = [
     {
-       type: "input",
-       message: "please enter employee's name",
-       name: "name",
+        type: "input",
+        message: "please enter Engineers's name",
+        name: "name",
 
     },
+    {
+        type: "input",
+        message: "enter Engineers GitHub username",
+        name: "github"
+    },
+
     {
         type: "input",
         message: "Please enter employee ID",
@@ -19,55 +58,6 @@ const employeeQuestions = [
         message: "please employee email",
         name: "email"
     }
-]
-
-const managerQuestions = [
-    {
-        type: "input",
-        message: "please enter Managers's name",
-        name: "name",
- 
-     },
-    {
-        type: "input",
-        message: "please enter your office number",
-        name: "officeNum"
-    },
-    {
-         type: "input",
-         message: "Please enter employee ID",
-         name: "employeeID"
-     },
-     {
-         type: "input",
-         message: "please employee email",
-         name: "email"
-     }
-]
-
-const engineerQuestions = [
-    {
-        type: "input",
-        message: "please enter Engineers's name",
-        name: "name",
- 
-     },
-    {
-        type: "input",
-        message: "enter Engineers GitHub username",
-        name: "github"
-    }
- 
-     {
-         type: "input",
-         message: "Please enter employee ID",
-         name: "employeeID"
-     },
-     {
-         type: "input",
-         message: "please employee email",
-         name: "email"
-     }
 ]
 
 const internQuestions = [
@@ -75,9 +65,9 @@ const internQuestions = [
         type: "input",
         message: "please enter Interns name",
         name: "name",
- 
-     },
-     {
+
+    },
+    {
         type: "input",
         message: "Please enter employee ID",
         name: "employeeID"
@@ -86,7 +76,7 @@ const internQuestions = [
         type: "input",
         message: "please employee email",
         name: "email"
-    }
+    },
     {
         type: "input",
         message: "enter interns school name",
@@ -98,26 +88,19 @@ const addMoreEmployees = [
     {
         type: "list",
         message: "do you want to add another employee",
-        choices:["Employee","Engineer", "Intern", "Finish"]
+        choices: ["Engineer", "Intern", "Finish"]
 
     }
 ]
 //add fuction to initilize and ask user first set of questions
 function addMoreEmployeeClass() {
     inqurier.prompt(addMoreEmployees).then((data) => {
-        console.log("this works")
-        console.log(data)
+
     })
 
 }
 
 function run() {
-    inqurier.prompt(employeeQuestions, managerQuestions).then((data) => {
-        console.log("this works")
-        console.log(data)
-    })
-    addMoreEmployeeClass()
-}
   
-
-run()
+   }
+   managerQuestions()
